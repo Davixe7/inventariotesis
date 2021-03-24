b8e6a1a8b1356d84a0d63754d469b6e99872fdfe
David <davidguilarte7@gmailcom>
2021-03-24 07:01:35 -0400

    ignoring public storage

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,59 +4,61 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        App\Rol::create([
-          'nombre' => 'Administrador'
-        ]);
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    App\Rol::create([
+      'nombre' => 'Administrador'
+    ]);
 
-        App\User::create([
-          'nombre'   => 'John Doe',
-          'usuario'  => 'johndoe',
-          'email'    => 'johndoe@mail.com',
-          'password' => bcrypt(123456),
-          'role_id'  => 1
-        ]);
+    App\User::create([
+      'nombre'   => 'John Doe',
+      'usuario'  => 'johndoe',
+      'email'    => 'johndoe@mail.com',
+      'password' => bcrypt(123456),
+      'role_id'  => 1
+    ]);
 
-        $proveedores = [
-          "N/A",
-          "INTERNO",
-          "LABTEST",
-          "CARESENS",
-          "BIOTECH",
-          "KAESER",
-          "EXTERNO",
-          "ANNAR",
-          "ORTHO CLINICAL",
-          "KAIKA",
-          "BIOREG",
-          "JOHNSON",
-          "BIOIN",
-          "FIRST MEDICAL",
-          "AIRE FRIO",
-          "ORTHOSYSTEM",
-          "FRESENIUS",
-          "GBARCO",
-          "SERVITRONICS",
-          "GENERAL ELECTRIC",
-          "INTERNMO",
-        ];
+    $proveedores = [
+      "N/A",
+      "INTERNO",
+      "LABTEST",
+      "CARESENS",
+      "BIOTECH",
+      "KAESER",
+      "EXTERNO",
+      "ANNAR",
+      "ORTHO CLINICAL",
+      "KAIKA",
+      "BIOREG",
+      "JOHNSON",
+      "BIOIN",
+      "FIRST MEDICAL",
+      "AIRE FRIO",
+      "ORTHOSYSTEM",
+      "FRESENIUS",
+      "GBARCO",
+      "SERVITRONICS",
+      "GENERAL ELECTRIC",
+      "INTERNMO",
+    ];
 
-        foreach($proveedores as $pro){
-          \App\Proveedor::create([
-            'nombre' => $pro,
-            'tipo' => 'mantenimiento'
-          ]);
-        }
-
-        \App\Clasificacion::create([
-          'nombre' => 'Prueba',
-          'descripcion' => 'Clasificación de prueba'
-        ]);
+    foreach ($proveedores as $pro) {
+      \App\Proveedor::create([
+        'nombre' => $pro,
+        'tipo' => 'mantenimiento'
+      ]);
     }
+
+    \App\Clasificacion::create([
+      'nombre' => 'Prueba',
+      'descripcion' => 'Clasificación de prueba'
+    ]);
+
+    DB::unprepared(file_get_contents(storage_path('app/ddbb.sql')));
+  }
 }
