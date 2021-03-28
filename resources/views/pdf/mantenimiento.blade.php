28823c877b9a3bbf8704d20668e09c91af767b90
David <davidguilarte7@gmailcom>
2021-03-28 07:55:29 -0400

    Latest Stable Version

--- resources/views/pdf/mantenimiento.blade.php
+++ resources/views/pdf/mantenimiento.blade.php
@@ -3,7 +3,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Reporte de compra</title>
+<title>Detalles del Mantenimiento</title>
 <style>
   body {
     font-family: Arial, sans-serif;
